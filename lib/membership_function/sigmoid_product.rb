77be9e323de81f0dffd8fb1ccfca670c4a0e805d
Franco Javier Salinas Mendoza <franco.salinas.mendoza@gmail.com>
2016-10-11 20:55:21 -0300

    Create the sigmoid product membership function class.

---
+++ lib/membership_function/sigmoid_product.rb
@@ -0,0 +1,67 @@
+module UFuzzyConvert
+
+  require_relative '../exception'
+
+  module MembershipFunction
+
+    require_relative 'tabulated'
+
+    class SigmoidProduct < Tabulated
+
+      #----------------------------[constants]---------------------------------#
+
+      PARAMETER_NUMBER = 4
+
+      #----------------------------[public class methods]----------------------#
+
+      #----------------------------[initialization]----------------------------#
+
+      ##
+      # Creates a membership function which is equal to the product of two
+      # sigmoidal functions.
+      #
+      # $$f(x) = {1 \over {1+e^{-a_1(x-c_1)}}} {1 \over {1+e^{-a_2(x-c_2)}}}$$
+      #
+      # @param [Numeric] a1
+      # @param [Numeric] c1
+      # @param [Numeric] a2
+      # @param [Numeric] c2
+      # @param [String] name
+      #   The name of the membership function.
+      # @raise [InputError]
+      #   When a1, c1, a2 or c2 have invalid values.
+      #
+      def initialize(a1, c1, a2, c2, name = "")
+        if (
+          not a1.is_a? Numeric or
+          not c1.is_a? Numeric or
+          not a2.is_a? Numeric or
+          not c2.is_a? Numeric
+        )
+          raise InputError.new, "Parameters must be numeric."
+        end
+
+        @name = name
+        @a1 = a1.to_f
+        @c1 = c1.to_f
+        @a2 = a2.to_f
+        @c2 = c2.to_f
+      end
+
+      #----------------------------[public methods]----------------------------#
+
+      def evaluate(x)
+        s1 = 1 / (1 + Math.exp(-@a1 * (x - @c1)))
+        s2 = 1 / (1 + Math.exp(-@a2 * (x - @c2)))
+        return s1 * s2
+      end
+
+      #----------------------------[private class methods]---------------------#
+
+      #----------------------------[private methods]---------------------------#
+
+    end
+
+  end
+
+end
