c2c563176a59c96fb3ee3c6f1b9872a698b97719
Franco Javier Salinas Mendoza <franco.salinas.mendoza@gmail.com>
2016-10-11 20:00:16 -0300

    Create the gaussian membership function class.

---
+++ lib/membership_function/gaussian.rb
@@ -0,0 +1,63 @@
+module UFuzzyConvert
+
+  require_relative '../exception'
+
+  module MembershipFunction
+
+    require_relative 'tabulated'
+
+    class Gaussian < Tabulated
+
+      #----------------------------[constants]---------------------------------#
+
+      PARAMETER_NUMBER = 2
+
+      #----------------------------[public class methods]----------------------#
+
+      #----------------------------[initialization]----------------------------#
+
+      ##
+      # Creates a membership function which is equal to the difference of two
+      # sigmoidal functions.
+      #
+      # $$f(x) = e^{-(x-c)^2 \over {2\sigma^2}}$$
+      #
+      # @param [Numeric] sig
+      # @param [Numeric] c
+      # @param [String] name
+      #   The name of the membership function.
+      # @raise [InputError]
+      #   When sig or c have invalid values.
+      #
+      def initialize(sig, c, name = "")
+        if (
+          not sig.is_a? Numeric or
+          not c.is_a? Numeric
+        )
+          raise InputError.new, "Parameters must be numeric."
+        end
+
+        if 2 * sig ** 2 == 0
+          raise InputError.new, "sig cannot be 0."
+        end
+
+        @name = name
+        @sig = sig
+        @c = c
+      end
+
+      #----------------------------[public methods]----------------------------#
+
+      def evaluate(x)
+        return Math.exp(-(x - @c) ** 2 / (2.0 * (@sig ** 2)))
+      end
+
+      #----------------------------[private class methods]---------------------#
+
+      #----------------------------[private methods]---------------------------#
+
+    end
+
+  end
+
+end
---
+++ test/membership_function/test_gaussian.rb
@@ -0,0 +1,49 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'rubygems'
+gem 'mocha'
+require 'test/unit'
+require 'mocha/test_unit'
+require './lib/membership_function/gaussian'
+include Mocha::API
+
+
+class GaussianTest < Test::Unit::TestCase
+
+  def test_invalid_parameters
+
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+      "Parameters must be numeric."
+    ) do
+      UFuzzyConvert::MembershipFunction::Gaussian.new "1", 2
+    end
+
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+      "Parameters must be numeric."
+    ) do
+      UFuzzyConvert::MembershipFunction::Gaussian.new 1, "2"
+    end
+
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+      "sig cannot be 0."
+    ) do
+      UFuzzyConvert::MembershipFunction::Gaussian.new 0, 2
+    end
+  end
+
+  def test_evaluate
+    function = UFuzzyConvert::MembershipFunction::Gaussian.new 2, 5
+
+    assert_in_delta(0.043937, function.evaluate(0), 0.043937 * 1e-4)
+    assert_in_delta(0.324652, function.evaluate(2), 0.324652 * 1e-4)
+    assert_in_delta(0.882497, function.evaluate(4), 0.882497 * 1e-4)
+    assert_in_delta(0.882497, function.evaluate(6), 0.882497 * 1e-4)
+    assert_in_delta(0.324652, function.evaluate(8), 0.324652 * 1e-4)
+    assert_in_delta(0.043937, function.evaluate(10), 0.043937 * 1e-4)
+  end
+
+end
