ca0b89d8e28d80a86c4acb7deedcba3e1fe60239
Franco Javier Salinas Mendoza <franco.salinas.mendoza@gmail.com>
2016-10-11 15:59:13 -0300

    Create the tabulated membership function class.

---
+++ lib/membership_function/tabulated.rb
@@ -0,0 +1,74 @@
+module UFuzzyConvert
+
+  require_relative '../exception'
+  require_relative '../fixed_point'
+
+  module MembershipFunction
+
+    class Tabulated
+
+      #----------------------------[constants]---------------------------------#
+
+      #----------------------------[public class methods]----------------------#
+
+      #----------------------------[initialization]----------------------------#
+
+      #----------------------------[public methods]----------------------------#
+
+      ##
+      # Converts the membership function into a CFS array.
+      #
+      # @param [Numeric] range_min
+      #   The minimum value that the variable is able to take.
+      # @param [Numeric] range_max
+      #   The maximum value that the variable is able to take.
+      # @param [Hash<Symbol>] options
+      # @option options [Integer] :tsize
+      #   Base 2 logarithm of the number of entries in a tabulated membership
+      #   function.
+      # @return [Array<Integer>]
+      #   Returns the membership function converted to CFS format.
+      #
+      def to_cfs(range_min, range_max, options)
+        if not range_min.is_a? Numeric
+          raise InputError.new, "Range lower bound must be a number."
+        end
+        
+        if not range_max.is_a? Numeric
+          raise InputError.new, "Range upper bound must be a number."
+        end
+
+        if range_max <= range_min
+          raise InputError.new, "Range bounds are swapped."
+        end
+
+        if not options[:tsize].is_a? Integer
+          raise InputError.new, "options[:tsize] must be integer."
+        end
+
+        if options[:tsize] > 16
+          raise InputError.new, "options[:tsize] must be less or equal to 16."
+        end
+
+        table_size = 1 << options[:tsize]
+
+        membership_function = Array.new
+
+        delta = range_max - range_min
+        for index in 0...table_size
+          x = range_min + (index + 0.5) * delta / table_size
+          membership_function.push(*evaluate(x).to_cfs(0, 1))
+        end
+
+        return membership_function
+      end
+
+      #----------------------------[private class methods]---------------------#
+
+      #----------------------------[private methods]---------------------------#
+
+    end
+
+  end
+
+end
---
+++ test/membership_function/test_tabulated.rb
@@ -0,0 +1,139 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'rubygems'
+gem 'mocha'
+require 'test/unit'
+require 'mocha/test_unit'
+require './lib/membership_function/tabulated'
+include Mocha::API
+
+
+class TabulatedTest < Test::Unit::TestCase
+
+  def test_to_cfs_invalid_range_type
+    function = UFuzzyConvert::MembershipFunction::Tabulated.new
+
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+      "Range lower bound must be a number."
+    ) do
+      function.to_cfs("asd", 20, {:tsize => 8})
+    end
+
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+      "Range upper bound must be a number."
+    ) do
+      function.to_cfs(-20, "asd", {:tsize => 8})
+    end
+  end
+
+  def test_to_cfs_range_swapped
+    function = UFuzzyConvert::MembershipFunction::Tabulated.new
+
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+      "Range bounds are swapped."
+    ) do
+      function.to_cfs(-20, -40, {:tsize => 8})
+    end
+  end
+
+  def test_to_cfs_invalid_table_size
+    function = UFuzzyConvert::MembershipFunction::Tabulated.new
+
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+      "options[:tsize] must be integer."
+    ) do
+      function.to_cfs(0, 1, {:tsize => "asd"})
+    end
+
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+      "options[:tsize] must be integer."
+    ) do
+      function.to_cfs(0, 1, {})
+    end
+  end
+
+  def test_to_cfs_table_size_too_big
+    function = UFuzzyConvert::MembershipFunction::Tabulated.new
+
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+        "options[:tsize] must be less or equal to 16."
+    ) do
+      function.to_cfs(0, 1, {:tsize => 17})
+    end
+  end
+
+  def test_to_cfs_linear_with_small_table
+    linear = UFuzzyConvert::MembershipFunction::Tabulated.new
+    linear.define_singleton_method(:evaluate) do |x|
+      return x
+    end
+
+    assert_equal(
+      linear.to_cfs(0, 1, {:tsize => 4}),
+      [
+        0x02, 0x00,
+        0x06, 0x00,
+        0x0A, 0x00,
+        0x0E, 0x00,
+
+        0x12, 0x00,
+        0x16, 0x00,
+        0x1A, 0x00,
+        0x1E, 0x00,
+
+        0x22, 0x00,
+        0x26, 0x00,
+        0x2A, 0x00,
+        0x2E, 0x00,
+
+        0x32, 0x00,
+        0x36, 0x00,
+        0x3A, 0x00,
+        0x3E, 0x00
+      ]
+    )
+  end
+
+  def test_to_cfs_trapezoidal_with_small_table
+    trapezoidal = UFuzzyConvert::MembershipFunction::Tabulated.new
+    trapezoidal.define_singleton_method(:evaluate) do |x|
+      if x < 1
+        return x
+      else
+        return 1
+      end
+    end
+
+    assert_equal(
+      trapezoidal.to_cfs(0, 2, {:tsize => 4}),
+      [
+        0x04, 0x00,
+        0x0C, 0x00,
+        0x14, 0x00,
+        0x1C, 0x00,
+
+        0x24, 0x00,
+        0x2C, 0x00,
+        0x34, 0x00,
+        0x3C, 0x00,
+
+        0x40, 0x00,
+        0x40, 0x00,
+        0x40, 0x00,
+        0x40, 0x00,
+
+        0x40, 0x00,
+        0x40, 0x00,
+        0x40, 0x00,
+        0x40, 0x00
+      ]
+    )
+  end
+end
