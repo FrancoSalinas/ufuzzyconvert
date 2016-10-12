186935cbfa0052486a0a2fafd95e5ee71febd4f8
Franco Javier Salinas Mendoza <franco.salinas.mendoza@gmail.com>
2016-10-11 21:53:38 -0300

    Create the trapezoidal membership function class.

---
+++ lib/membership_function/trapezoidal.rb
@@ -0,0 +1,63 @@
+module UFuzzyConvert
+
+  require_relative '../exception'
+
+  module MembershipFunction
+
+    require_relative 'non_tabulated'
+
+    class Trapezoidal < NonTabulated
+
+      #----------------------------[constants]---------------------------------#
+
+      PARAMETER_NUMBER = 4
+
+      #----------------------------[public class methods]----------------------#
+
+      #----------------------------[initialization]----------------------------#
+
+      ##
+      # Creates a trapezoidal membership function.
+      #
+      # @param [Numeric] x1
+      #   The x-coordinate of the first vertex.
+      # @param [Numeric] x2
+      #   The x-coordinate of the second vertex.
+      # @param [Numeric] x3
+      #   The x-coordinate of the third vertex.
+      # @param [Numeric] x4
+      #   The x-coordinate of the fourth vertex.
+      # @param [String] name
+      #   The name of the membership function.
+      # @raise [InputError]
+      #   When x1, x2, x3 or x4 have invalid values.
+      #
+      def initialize(x1, x2, x3, x4, name = "")
+        if (
+          not x1.is_a? Numeric or
+          not x2.is_a? Numeric or
+          not x3.is_a? Numeric or
+          not x4.is_a? Numeric
+        )
+          raise InputError.new, "Parameters must be numeric."
+        end
+
+        if x1 > x2 or x2 > x3 or x3 > x4
+          raise InputError.new, "Parameters are not ordered."
+        end
+
+        @name = name
+        @xs = [x1, x2, x3, x4]
+      end
+
+      #----------------------------[public methods]----------------------------#
+
+      #----------------------------[private class methods]---------------------#
+
+      #----------------------------[private methods]---------------------------#
+
+    end
+
+  end
+
+end
---
+++ test/membership_function/test_trapezoidal.rb
@@ -0,0 +1,81 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'rubygems'
+gem 'mocha'
+require 'test/unit'
+require 'mocha/test_unit'
+require './lib/membership_function/trapezoidal'
+include Mocha::API
+
+
+class TrapezoidalTest < Test::Unit::TestCase
+
+  def test_to_cfs_invalid_range_type
+
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+      "Parameters must be numeric."
+    ) do
+      UFuzzyConvert::MembershipFunction::Trapezoidal.new "1", 2, 3, 4
+    end
+
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+      "Parameters must be numeric."
+    ) do
+      UFuzzyConvert::MembershipFunction::Trapezoidal.new 1, "2", 3, 4
+    end
+
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+      "Parameters must be numeric."
+    ) do
+      UFuzzyConvert::MembershipFunction::Trapezoidal.new 1, 2, "3", 4
+    end
+
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+      "Parameters must be numeric."
+    ) do
+      UFuzzyConvert::MembershipFunction::Trapezoidal.new 1, 2, 3, "4"
+    end
+  end
+
+  def test_parameters_not_ordered
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+      "Parameters are not ordered."
+    ) do
+      UFuzzyConvert::MembershipFunction::Trapezoidal.new 2, 1, 3, 4
+    end
+
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+      "Parameters are not ordered."
+    ) do
+      UFuzzyConvert::MembershipFunction::Trapezoidal.new 1, 3, 2, 4
+    end
+
+    assert_raise_with_message(
+      UFuzzyConvert::InputError,
+      "Parameters are not ordered."
+    ) do
+      UFuzzyConvert::MembershipFunction::Trapezoidal.new 1, 2, 4, 3
+    end
+  end
+
+  def test_to_cfs
+    function = UFuzzyConvert::MembershipFunction::Trapezoidal.new(-8, -4, 4, 8)
+
+    assert_equal(
+      function.to_cfs(-8, 8),
+      [
+        0x00, 0x00,
+        0x10, 0x00,
+        0x30, 0x00,
+        0x40, 0x00
+      ]
+    )
+  end
+end
