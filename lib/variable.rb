cdb032ceb1467cc14a1b6cce7e59fa917c97c2cf
Franco Javier Salinas Mendoza <franco.salinas.mendoza@gmail.com>
2016-10-20 12:18:14 -0300

    Create the variable module.

---
+++ lib/variable.rb
@@ -0,0 +1,40 @@
+module UFuzzyConvert
+
+  module Variable
+
+    require_relative 'exception'
+
+    def range_from_fis_data(variable_data)
+
+      param_data = variable_data.fetch(:parameters) {
+        raise InputError.new, "No parameters found. Range is required."
+      }
+
+      range = param_data.fetch(:Range) {
+        raise InputError.new, "Range not defined."
+      }
+
+      if range.length != 2
+        raise InputError.new, "Range matrix must have two elements."
+      end
+
+      return range[0], range[1]
+    end
+
+    def membership_functions_from_fis_data(variable_data)
+      membership_functions = Array.new
+
+      membership_data_list = variable_data[:membership]
+      unless membership_data_list.nil?
+        membership_data_list.each do |index, membership_data|
+          membership_functions.push(
+            MembershipFunction.from_fis_data membership_data
+          )
+        end
+      end
+
+      return membership_functions
+    end
+  end
+
+end
